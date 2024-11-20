6cdffd38fb43780ec48315ac8d0721b0f609494d
Filipe Mendes <fmatiasmendes@gmail.com>
2024-11-20 16:01:59 +0000

    homepage created

---
+++ home.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Stock Management</title>
+		<link rel="shortcut icon" href="images/background.jpg" type="image/x-icon">
+		<link rel="stylesheet" href="styles/style.css">
+	</head>
+
+	<body>
+
+		<header>
+
+			<div class="outer-border">
+				<h1 class="main-title">STOCK MANAGEMENT</h1>
+				<nav>
+					<ul class="nav-links">
+						<li><a href="">HOME</a></li>
+						<li><a href="">PRODUCTS LIST</a></li>
+						<li><a href="">CODE SEARCH</a></li>
+						<li><a href="">REGISTER PRODUCT</a></li>
+						<li><a href="">EDICT PRODUCT</a></li>
+						<li><a href="">DELETE PRODUCT</a></li>
+						<li><a href="">REGISTER SALES</a></li>
+						<li><a href="">LOGOUT</a></li>
+					</ul>
+				</nav>				
+			</div>
+
+		</header>
+		
+		<main></main>
+
+		<footer></footer>	
+			
+	</body>
+</html>
