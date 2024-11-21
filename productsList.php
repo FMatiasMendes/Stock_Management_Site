62cd73c6d6520793fd8e43d700a1681938a0b1a1
Filipe Mendes <fmatiasmendes@gmail.com>
2024-11-21 11:16:35 +0000

    productsList.php created

---
+++ productsList.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Stock Management</title>
+		<link rel="shortcut icon" href="images/background.jpg" type="image/x-icon">
+		<link rel="stylesheet" href="styles/style.css">
+	</head>
+
+	<body>
+
+		<header>
+
+			<div class="outer-border">
+				<h1 class="main-title">STOCK MANAGEMENT</h1>
+				<nav>
+					<ul class="nav-links">
+						<li><a href="">HOME</a></li>
+						<li><a href="">PRODUCTS LIST</a></li>
+						<li><a href="">CODE SEARCH</a></li>
+						<li><a href="">REGISTER PRODUCT</a></li>
+						<li><a href="">EDICT PRODUCT</a></li>
+						<li><a href="">DELETE PRODUCT</a></li>
+						<li><a href="">REGISTER SALES</a></li>
+						<li><a href="">LOGOUT</a></li>
+					</ul>
+				</nav>				
+			</div>
+
+		</header>
+		
+		<main>
+
+			<div class="outer-border">
+				<!-- php edition missing (name) -->
+				<p class="welcome-message">Welcome Filipe!</p>
+				<!-- php edition missing (date) -->
+				<p class="last-login-message">Last time you were here: dd/mm/yyyy at xx:xxH.</p>
+			</div>
+
+		</main>
+
+		<footer>
+			<a href="https://www.linkedin.com/in/fmatiasmendes/" target="_blank">
+				&copy;2024 FILIPE MENDES. ALL RIGHTS RESERVED
+			</a>
+		</footer>	
+			
+	</body>
+</html>
