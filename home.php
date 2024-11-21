70e2be3eae72fc084e10035b5af210f2f77aa291
Filipe Mendes <fmatiasmendes@gmail.com>
2024-11-21 12:35:40 +0000

    productsList html page done

--- home.php
+++ home.php
@@ -42,6 +42,7 @@
 		</main>
 
 		<footer>
+			<!-- need to fix the position with less height -->
 			<a href="https://www.linkedin.com/in/fmatiasmendes/" target="_blank">
 				&copy;2024 FILIPE MENDES. ALL RIGHTS RESERVED
 			</a>
--- productsList.php
+++ productsList.php
@@ -31,17 +31,75 @@
 		</header>
 		
 		<main>
-
+		<!-- consider make the ascending/descending order with arrows on each column -->
 			<div class="outer-border">
-				<!-- php edition missing (name) -->
-				<p class="welcome-message">Welcome Filipe!</p>
-				<!-- php edition missing (date) -->
-				<p class="last-login-message">Last time you were here: dd/mm/yyyy at xx:xxH.</p>
+				<h3 class="page-titles">PRODUCTS LIST</h3>				
+				<div class="column-selector">
+					<label for="">Column</label>
+					<select name="column" id="column">
+						<option value="id">ID</option>
+						<option value="name">NAME</option>
+						<option value="price">PRICE</option>
+						<option value="quantity">QUANTITY</option>
+					</select>
+				</div>
+				<div class="radio-inputs">
+					<div>
+						<label for="">Ascending</label>
+						<br>
+						<input type="radio" name="order" value="ascending" checked="checked">
+					</div>
+					<div>
+						<label for="">Descending</label>
+						<br>
+						<input type="radio" name="order" value="descending">
+					</div>
+				</div>
+				<input type="submit" name="" value="LIST" class="list-button">	
+			
+				<table>
+					<tr>
+						<th>ID</th>
+						<th class="name-column">NAME</th>
+						<th>PRICE</th>
+						<th>QUANTITY</th>
+					</tr>
+					<tr>
+						<td>1</td>
+						<td>Green Pen</td>
+						<td>1.30 €</td>
+						<td>53</td>
+					</tr>
+					<tr>
+						<td>2</td>
+						<td>Pink Pen</td>
+						<td>0.75 €</td>
+						<td>10</td>
+					</tr>
+					<tr>
+						<td>48</td>
+						<td>White wallet</td>
+						<td>6.50 €</td>
+						<td>12</td>
+					</tr>
+					<tr>
+						<td>49</td>
+						<td>Black wallet</td>
+						<td>8.99 €</td>
+						<td>15</td>
+					</tr>
+					<tr>
+						<td>50</td>
+						<td>Pencil Case</td>
+						<td>1.49 €</td>
+						<td>50</td>
+					</tr>
+				</table>
 			</div>
-
 		</main>
 
 		<footer>
+			<!-- need to fix the position with less height -->
 			<a href="https://www.linkedin.com/in/fmatiasmendes/" target="_blank">
 				&copy;2024 FILIPE MENDES. ALL RIGHTS RESERVED
 			</a>
