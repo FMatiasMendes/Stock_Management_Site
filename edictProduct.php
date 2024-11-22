c6969d39b115faa71c6cf47fb3ef5309876914ce
Filipe Mendes <fmatiasmendes@gmail.com>
2024-11-22 12:24:30 +0000

    edict page working. Still missing make the div appear/disappear.

--- edictProduct.php
+++ edictProduct.php
@@ -1,5 +1,36 @@
 <?php
+
+	require("functions/productsFunctions.php");
+
 	$page = "EDICT PRODUCT";
+	$listProducts = listProducts();
+	$id = null;
+	$product = null;
+
+	if (isset($_GET["edict"])) {
+    $id = filter_input(INPUT_GET, "edict", FILTER_VALIDATE_INT);
+
+    if ($id !== false) {
+			$product = getProductById($id);
+    }
+	}	
+
+	$form = isset($_GET["name"]) && isset($_GET["price"]) && isset($_GET["quantity"]);
+
+	if ($form){
+		$name = filter_input(INPUT_GET, 'name', FILTER_SANITIZE_STRING);
+		$price = filter_input(INPUT_GET, 'price', FILTER_VALIDATE_FLOAT);
+		$quantity = filter_input(INPUT_GET, 'quantity', FILTER_VALIDATE_INT);
+		$id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+
+		if($name !== false && $price !== false && $quantity !== false && $id !== false){
+			edictProduct($name, $price, $quantity, $id);
+
+			header("Location: edictProduct.php");
+      exit();
+		}
+	}
+
 ?>
 
 <!DOCTYPE html>
@@ -21,19 +52,18 @@
 		<main>
 
 		<div class="outer-border">
-			<!-- change id number -->
-			<h3 class="page-titles">EDICT PRODUCT (48)</h3>
+			<!-- make div appear/disappear -->
+			<h3 class="page-titles">EDICT PRODUCT (<?= htmlspecialchars($product["id"] ?? ''); ?>)</h3>
 			<div>
-				<form action="" class="search-form">
-					<!-- change placeholders number -->
-					<input type="text" name="name" placeholder="Green Pen"><br><br>					
-					<input type="number" name="price" placeholder="1.68 €"><br><br>
-					<input type="number" name="quantity" placeholder="153"><br><br>
-					<input type="submit" name="" value="SAVE" class="save-button">
-					<button class="cancel-button">CANCEL</button>
+				<form action="edictProduct.php" class="search-form" method="get">
+					<input type="hidden" name="id" value="<?= $product['id'] ?? ''; ?>">
+					<input type="text" name="name" placeholder="Name" value="<?= $product["name"] ?? ''; ?>" required autofocus><br><br>					
+					<input type="number" name="price" placeholder="Price (€)" value="<?= $product["price"] ?? ''; ?>" min="0" max="999999" step="0.01" required><br><br>
+					<input type="number" name="quantity" placeholder="Quantity" value="<?= $product["quantity"] ?? ''; ?>" min="0" max="999999" step="1" required><br><br>
+					<input type="submit" name="save" value="SAVE" class="save-button">
+					<a href="edictProduct.php" class="cancel-button">CANCEL</a>
 				</form>
-			</div>				
-
+			</div>
 		</div>
 
 			<div class="outer-border">
@@ -47,61 +77,20 @@
 						<th>QUANTITY</th>
 						<th>ACTION</th>
 					</tr>
-					<tr>
-						<td>1</td>
-						<td>Green Pen</td>
-						<td>1.30 €</td>
-						<td>53</td>
-						<td>
-							<form action="">
-								<input type="submit" name="edict" value="EDICT" class="edict-button">
-							</form>							
-						</td>
-					</tr>
-					<tr>
-						<td>2</td>
-						<td>Pink Pen</td>
-						<td>0.75 €</td>
-						<td>10</td>
-						<td>
-							<form action="">
-								<input type="submit" name="edict" value="EDICT" class="edict-button">
-							</form>							
-						</td>
-					</tr>
-					<tr>
-						<td>48</td>
-						<td>White wallet</td>
-						<td>6.50 €</td>
-						<td>12</td>
-						<td>
-							<form action="">
-								<input type="submit" name="edict" value="EDICT" class="edict-button">
-							</form>							
-						</td>
-					</tr>
-					<tr>
-						<td>49</td>
-						<td>Black wallet</td>
-						<td>8.99 €</td>
-						<td>15</td>
-						<td>
-							<form action="">
-								<input type="submit" name="edict" value="EDICT" class="edict-button">
-							</form>							
-						</td>
-					</tr>
-					<tr>
-						<td>50</td>
-						<td>Pencil Case</td>
-						<td>1.49 €</td>
-						<td>50</td>
-						<td>
-							<form action="">
-								<input type="submit" name="edict" value="EDICT" class="edict-button">
-							</form>							
-						</td>
-					</tr>
+
+					<?php foreach ($listProducts as $i => $l): ?>
+						<tr>
+							<td><?= htmlspecialchars($l["id"]); ?></td>
+							<td><?= htmlspecialchars($l["name"]); ?></td>
+							<td><?= number_format($l["price"], 2); ?> €</td>
+							<td><?= htmlspecialchars($l["quantity"]); ?></td>
+							<td>
+							<form action="edictProduct.php" method="get">
+								<button type="submit" name="edict" value="<?= $l["id"]; ?>" class="edict-button">EDICT</button>
+							</form>
+						</tr>
+					<?php endforeach; ?>
+
 				</table>
 			</div>
 		</main>
--- functions/productsFunctions.php
+++ functions/productsFunctions.php
@@ -13,10 +13,21 @@ function insertProduct($name, $price, $quantity){
   $insert = iduSQL("INSERT INTO products (name, price, quantity) VALUES ('$name', '$price', '$quantity')");
 }
 
-//to get specific product
+//to get specific product by name
 function getProduct($name){
   $result = selectSpecificSQL("SELECT * FROM products WHERE name='$name'");
   return $result;
+}
+
+//to get specific product by ID
+function getProductById($id){
+  $result = selectSpecificSQL("SELECT * FROM products WHERE id='$id'");
+  return $result;
+}
+
+//to edict a product
+function edictProduct($name, $price, $quantity, $id){
+  iduSQL("UPDATE products SET name='$name', price='$price', quantity='$quantity' WHERE id='$id'");
 }
 
 ?>
