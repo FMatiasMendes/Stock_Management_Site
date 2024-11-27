50bcf0ecd3d2152c451344f7298a1a18ccccbf68
Filipe Mendes <fmatiasmendes@gmail.com>
2024-11-27 11:11:19 +0000

    comments deleted on functions folder

--- functions/loginFunction.php
+++ functions/loginFunction.php
@@ -6,18 +6,17 @@
 	date_default_timezone_set("Europe/Lisbon");
 
 	function login($user, $pass){
-			$logged_user = selectSpecificSQL("SELECT * FROM users WHERE user='$user' AND password='$pass'"); 
-			
-			if(!empty($logged_user)){
+		$logged_user = selectSpecificSQL("SELECT * FROM users WHERE user='$user' AND password='$pass'"); 
+		
+		if(!empty($logged_user)){
+			$_SESSION["logged_user"] = $logged_user;
+			$id = $logged_user["id"];
+			$date = date("d/m/Y - H:i:s");
 
-					$_SESSION["logged_user"] = $logged_user;
-					$id = $logged_user["id"];
-					$date = date("d/m/Y - H:i:s");
-
-					iduSQL ("UPDATE users SET last_access='$date' WHERE id='$id'");
-					return true;
-			}			
-			return false;
+			iduSQL ("UPDATE users SET last_access='$date' WHERE id='$id'");
+			return true;
+		}			
+		return false;
 	}
 
 	function loginStatus(){
